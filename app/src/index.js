f56e74b659054268eceee1e1d37297cdfc714c45
Jujhar Singh <jujhar@jujhar.com>
2021-07-05 12:35:22 +0100

    almost functional db

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,26 @@
 import { data } from "./js/data.js";
+// TODO get live rates from curl 'https://api.octopus.energy/v1/products/GO-21-05-13/electricity-tariffs/E-1R-GO-21-05-13-E/standard-unit-rates/' \
+const cheapPeriodPrice = 5;
+const peakPeriodPrice = 15.64;
+
+const cheapPeriodCost = data.cheapPeriodConsumption * cheapPeriodPrice;
+const peakPeriodCost = data.peakPeriodConsumption * peakPeriodPrice;
 
 const template = `
 <p>Total Samples: ${data.meta.recordCount}</p>
 <p>Start Datetime: ${data.meta.startDateTime}</p>
 <p>End Datetime: ${data.meta.endDateTime}</p>
+<p>Total Usage in period: ${data.periodTotalKwh.toFixed(4)} kWh</p>
+<p>Cheap Period Consumption <em>(@ ${cheapPeriodPrice}p/kWh)</em>: ${data.cheapPeriodConsumption.toFixed(
+  2
+)} kWh | £${(cheapPeriodCost/100).toFixed(2)}</p>
+<p>Peak Period Consumption <em>(@ ${peakPeriodPrice}p/kWh)</em>: ${data.peakPeriodConsumption.toFixed(
+  2
+)} kWh | £${(peakPeriodCost/100).toFixed(2)}</p>
 `;
 
 document.getElementById("container").innerHTML = template;
 
-document.getElementById("debug").innerHTML = `<code>${JSON.stringify(
-  data
-)}</code>`;
+// document.getElementById("debug").innerHTML = `<code>${JSON.stringify(
+//   data
+// )}</code>`;
--- app/src/js/data.js
+++ app/src/js/data.js
@@ -1,16 +1,27 @@
 import raw from "../data/data.json";
 
-const startDateTime = "2022-07-02T23:00:00+01:00";
-const endDateTime = "2021-07-05T00:30:00+01:00";
+const startDateTime = raw.results.map((el) => el.interval_start).slice(-1);
+const endDateTime = raw.results.map((el) => el.interval_end)[0];
+const periodTotalKwh = raw.results
+  .map((el) => el.consumption)
+  .reduce((acc = 0, cur) => {
+    return acc + cur;
+  });
+// sum the values between the hours of 00:30 and 04:30
+const cheapPeriodConsumption = 3;
+// subtract cheapPeriod from total consumption to give us rest of usage
+const peakPeriodConsumption = periodTotalKwh - cheapPeriodConsumption;
 
 const data = {
   raw,
-  periodTotalKw: 1221,
+  periodTotalKwh,
   meta: {
     recordCount: raw.count,
     startDateTime,
     endDateTime,
   },
+  cheapPeriodConsumption,
+  peakPeriodConsumption,
 };
 
 export { data };
